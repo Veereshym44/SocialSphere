88268a1455dc3345e30b08588cd8cc6db0a83be2
Veeresh Kumar Y M <1si20cs126@sit.ac.in>
2023-04-07 21:00:40 +0530

    changed index

--- server/index.js
+++ server/index.js
@@ -1,29 +1,35 @@
+require('dotenv').config();
 const express=require('express');
-const cors=require('cors')
+const cors=require('cors');
 const mongoose=require('mongoose');
-require('dotenv').config()
-const app=express()
+
+const app=express();
 const PORT=5000;
-const MONGO_URI=process.env.MONGO_URI
-mongoose.connect(MONGO_URI,{
+const MONGO_URI=process.env.MONGO_URI;
+console.log(MONGO_URI)
+
+mongoose.connect(process.env.MONGO_URI,{
     useNewUrlParser:true,
     useUnifiedTopology:true
 });
+
 mongoose.connection.on('connected',()=>{
     console.log("success");
-})
+});
+
 mongoose.connection.on('error',(err)=>{
     console.log(err);
-})
+});
+
 app.use(express.static("public"));
-app.use(express.json())
-app.use(cors())
-require('./models/user')
-require('./models/post')
+app.use(express.json());
+app.use(cors());
+require('./models/user');
+require('./models/post');
 app.use(express.urlencoded({ extended: true }));
-app.use(require('./routes/auth'))
-app.use(require('./routes/post'))
+app.use(require('./routes/auth'));
+app.use(require('./routes/post'));
 
 app.listen(PORT,()=>{
     console.log("server");
-})
+});
